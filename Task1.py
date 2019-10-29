34872861d6b6fb0e8a8952c81af34c429042a1a3
Yauheni_Matveichyk <yauheni_matveichyk@epam.com>
2019-10-29 19:48:43 +0300

    2-nd commit

--- Task1.py
+++ Task1.py
@@ -1,16 +1,13 @@
 import platform
 import os
 import sys
-import site
 import json
-import pkg_resources
 import yaml
-import subprocess
 import pip
 from distutils.sysconfig import get_python_lib
+from pip._internal.operations.freeze import freeze
 
 py_pack = []
-from pip._internal.operations.freeze import freeze
 for requirement in freeze(local_only=True):
     py_pack.append(requirement)
     with open('Output.json', 'w', encoding='utf-8') as outfile:
@@ -22,7 +19,7 @@ for requirement in freeze(local_only=True):
             'PYTHONPATH': os.environ['PYTHONPATH'].split(os.pathsep),
             'Packages location': get_python_lib(),
             'Packages with version': py_pack,
-            }}, outfile, ensure_ascii=False, indent=2)
+        }}, outfile, ensure_ascii=False, indent=2)
 
     with open('Output.yml', 'w') as outfile:
         yaml.dump({'Python Checker': {
@@ -33,4 +30,4 @@ for requirement in freeze(local_only=True):
             'PYTHONPATH': os.environ['PYTHONPATH'].split(os.pathsep),
             'Packages location': get_python_lib(),
             'Packages with version': py_pack,
-            }}, stream=outfile, default_flow_style=False, indent=3)
+        }}, stream=outfile, default_flow_style=False, indent=3)
