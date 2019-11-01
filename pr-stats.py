3795a7abb60bd19b96eef002d58b99c0972c93bf
Yauheni_Matveichyk <yauheni_matveichyk@epam.com>
2019-11-01 20:38:01 +0300

    args added

--- pr-stats.py
+++ pr-stats.py
@@ -2,8 +2,9 @@
 import requests
 import argparse
 import json
+import datetime
 
-github_login = 'yauheni_matveichyk@epam.com'
+github_login = input("Enter your GitHub login:")
 github_password = getpass.getpass()
 
 parser = argparse.ArgumentParser()
@@ -11,10 +12,18 @@ parser.add_argument('--version', help='Application version.',
                     action='store_true')
 parser.add_argument('--state', help='Time, Show PR, state, login',
                     action='store_true')
+parser.add_argument('--week', help='Week number.',
+                    action='store_true')
+parser.add_argument('--day', help='On which day of the week',
+                    action='store_true')
+parser.add_argument('--count', help='How many',
+                    action='store_true')
+parser.add_argument('--all', help='How many',
+                    action='store_true')
 args = parser.parse_args()
 
 if args.version:
-    print('0.1_UNSTABLE')
+    print('0.2_UNSTABLE')
     quit()
 
 session = requests.Session()
@@ -26,14 +35,59 @@ data = json.loads(r.text)
 
 ss = r.content
 r.json()
-tt = json.loads(ss)
-r.status_code
+#tt = json.loads(ss)
+tt = r.json()
+# r.status_code
+
+if args.count:
+    count = args.count
+else:
+    count = 25
+
+for i in range(count):
+    user = '-'
+    week = '-'
+    day = '-'
+    state = '-'
+    hours = '-'
+    name = tt[i]['title']
+    creation = tt[i]['created_at'][:10]
+
 for item in tt:
     if args.state:
         a = item['title']
         x = item['state']
         o = item['created_at']
         y = item['user']['login']
-        print(o + ':' + a + ':' + x + ':' + y)
+        print(':Created at:' + o + ':Title:' + a + ':State:' + x + ':Login:' + y)
+
+    if args.week:
+        a = item['title']
+        x = item['state']
+        o = item['created_at']
+        y = item['user']['login']
+        week = datetime.datetime.strptime(creation, '%Y-%m-%d').isocalendar()[1]
+        print("Number of week: {0}".format(week) +
+              ':Created at:' + o + ':Title:' + a + ':State:' + x + ':Login:' + y)
+
+    if args.day:
+        a = item['title']
+        x = item['state']
+        o = item['created_at']
+        y = item['user']['login']
+        day = datetime.datetime.strptime(creation, '%Y-%m-%d').strftime('%A')
+        print('On which day of the week: {0}'.format(day) +
+              ':Created at:' + o + ':Title:' + a + ':State:' + x + ':Login:' + y)
+
+    if args.all:
+        a = item['title']
+        x = item['state']
+        o = item['created_at']
+        y = item['user']['login']
+        week = datetime.datetime.strptime(creation, '%Y-%m-%d').isocalendar()[1]
+        day = datetime.datetime.strptime(creation, '%Y-%m-%d').strftime('%A')
+        # print("On which day of the week: {0}".format(day))
+        print("Week number: {0}".format(week) + ":On which day of the week: {0}".format(day) +
+              ':Created at:' + o + ':Title:' + a + ':State:' + x + ':Login:' + y)
 
 session.close()
